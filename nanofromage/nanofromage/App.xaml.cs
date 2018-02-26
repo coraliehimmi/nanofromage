17c778a11e6c911b6e483aa394b33f515aad71e0
Coralie Himmi <coralie.himmi@evolution-xy.fr>
2018-02-26 16:20:32 +0100

    derniers ajouts

--- nanofromage/nanofromage/App.xaml.cs
+++ nanofromage/nanofromage/App.xaml.cs
@@ -22,8 +22,13 @@ namespace nanofromage
             MainWindow.Content = new FirstConnexion();
             MainWindow.Show();
 
-            Database<Character> Db = new Database<Character>();
-            Db.Insert(new Character());
+            //Database<Character> Db = new Database<Character>();
+            //Db.Insert(new Character());
+            Mage c1 = new Mage();
+            c1.Name = "mage";
+            c1.Level = 1;
+            c1.MagicPoint = 50;
+            c1.HitPoint = 10;
    
         }
     }
